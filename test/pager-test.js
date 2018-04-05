23df3d0dbc920f5f9605249ae43bdc7567b6c5e4
Steve Masterman <steve@vermonster.com>
2018-04-05 15:32:55 -0400

    add Pager class with nextPage and prevPage methods

---
+++ test/pager-test.js
@@ -0,0 +1,37 @@
+/* eslint-disable func-names */
+
+const fs = require('fs');
+const path = require('path');
+
+const { expect } = require('chai');
+const { Pager } = require('../lib/pager');
+
+function readFixture(filename) {
+  return JSON.parse(fs.readFileSync(path.normalize(`${__dirname}/fixtures/${filename}`, 'utf8')));
+}
+
+const mockHttpClient = {
+  get: url => url,
+};
+
+describe('Pager', () => {
+  beforeEach(function () {
+    this.pager = new Pager(mockHttpClient);
+  });
+
+  describe('#nextPage', () => {
+    it('returns httpClient get for the next link', function () {
+      const results = readFixture('search-results-page-1.json');
+      const url = 'https://sb-fhir-stu3.smarthealthit.org/smartstu3/open?_getpages=678cd733-8823-4324-88a7-51d369cf78a9&_getpagesoffset=3&_count=3&_pretty=true&_bundletype=searchset';
+      expect(this.pager.nextPage(results)).to.equal(url);
+    });
+  });
+
+  describe('#prevPage', () => {
+    it('returns httpClient get for the previous link', function () {
+      const results = readFixture('search-results-page-2.json');
+      const url = 'https://sb-fhir-stu3.smarthealthit.org/smartstu3/open?_getpages=678cd733-8823-4324-88a7-51d369cf78a9&_getpagesoffset=0&_count=3&_pretty=true&_bundletype=searchset';
+      expect(this.pager.prevPage(results)).to.equal(url);
+    });
+  });
+});

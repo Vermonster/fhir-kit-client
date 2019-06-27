ed81a3e7069588501c05600d4eb26b5cb3f68b36
Vicki Strateman <vicki@vermonster.com>
2019-06-27 14:53:44 -0400

    get patient data to render

--- examples/react-example/src/App.js
+++ examples/react-example/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Client from 'fhir-kit-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    patientData: null
+  }
+
+  //const patient = await fhirClient.read({ resourceType: 'Patient', id: token.patient });
+  async componentDidMount() {
+    
+    const baseUrl = "https://r4.smarthealthit.org";
+    const patientId = "62acbbe9-7bae-4d33-b53f-a389c1a0e281";
+    const client = new Client({ baseUrl });
+    
+    const response = await client.read({ resourceType: 'Patient', id: patientId });
+  
+    // const stringified = JSON.stringify(data);
+    this.setState({ patientData: response });
+  }
+
+  render() {
+    console.log(this.state.patientData);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Below, the app fetches a patient's data on <code>componentDidMount()</code></p>
+          {
+            this.state.patientData ?
+            <div>{ JSON.stringify(this.state.patientData) }</div> :
+            <div>No patient data</div>
+          }
+        </header>
+      </div>
+    );
+  }
 }
+  
 
 export default App;

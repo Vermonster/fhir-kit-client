49f3fe1ba57cabbdf3e0e60eac401372642569ac
Oliver Egger <oliver.egger@ahdis.ch>
2021-04-05 13:22:02 +0200

    change order of parameter for post

--- test/client-test.js
+++ test/client-test.js
@@ -538,6 +538,22 @@ describe('Client', function () {
         await this.fhirClient.operation({ name: 'everything' });
       });
 
+      it('runs system-level POST operation convert with input', async function () {
+        const patient = readFixture('patient.json');
+        nock(this.baseUrl)
+          .matchHeader('accept', 'application/fhir+json')
+          .post('/$convert')
+          .reply(200, function (uri, requestBody) {
+            return requestBody;
+          });
+
+        let response = await this.fhirClient.operation({ name: 'convert', method: 'post', input: patient });
+        console.log(response);
+
+        expect(response.resourceType).to.equal('Patient');
+        expect(response.id).to.equal('eb3271e1-ae1b-4644-9332-41e32c829486');
+      });
+      
       it('runs system-level GET operation', async function () {
         nock(this.baseUrl)
           .matchHeader('accept', 'application/fhir+json')

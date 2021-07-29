e7783795cd61c5029f3baee76dc334d1e6c5cbdc
ericfuxealth <eric.fu@xealth.io>
2021-07-29 13:14:42 -0700

    Fix lint errors

--- test/request-signer-test.js
+++ test/request-signer-test.js
@@ -10,7 +10,7 @@ describe('Client with request signer', function () {
     Host: 'healthlake.us-west-2.amazonaws.com',
     'X-Amz-Security-Token': 'IQoJb3JpZ2luX2VjEIv//////////wEaCXVzLXdlc3QtMiJGMEQCIGjCEk2H+cRZxyQd9xTH/3lQWo+/zG693Mf0KeYCuNxDAiBLK8CMZNR50ciEsCxv9zkjChBHhIFuN2jzhE4mNqFZ2Sq4AgiU//////////8BEAIaDDM3MjA4MDM3MDYwMiIMrmweyrv/bY0XttiS+MVnKBNjY4F167TmNahA8aSpjHFin89wRKaScV7iCGHbzKji7uYpLkEzXt79FAZQsnSRBOIGD7lqP6xb/lb3EvWTOCopn2+aOdAtSkJe/IWO960d3TeXyv7VUvtIVnLHgQn1F8hUfpVW3AzjkSJ9aiKQ2NWXvTPooY79nBiilpEUnQqY74QVnCsaquek80UYsjCCN63sZIaos7RTII25YVneb0vMZhle/+Hxnd8DcNzUbbcbwvE6VmtLsz8r2SCqQnOcSgbhhNOt/kd8sxcAzZzB6/6TTP2q1fwzd40+V3Kol9FDC+/IuIBjqeAXUgaC1VPK1ep5XJVIioQjiZDDwF7H4oDkNT8IxryioOa/WNN9zWSxbvBJr+CAtAZJxJa1wUuXgGUNYKMbEHLsBtkSPnHUgxyomdGVZmtC5hgWRNtcNxK9bhps/Hmvs5GmBXJznjsnKhJ8Zkh/DzADh/SyzzUovSXTi8n7DQ/9a6nYu7pofcNMO187u8fIOBLGX2i9IfnOW9xMtYtetr',
     'X-Amz-Date': '20210729T192323Z',
-    Authorization: 'AWS4-HMAC-SHA256 Credential=ASIAVNINX4OVBSUDTHU4/20210729/us-west-2/healthlake/aws4_request, SignedHeaders=host;x-amz-date;x-amz-security-token, Signature=95ab2c54f7c7c851e3cc6c6e2406460caf3a2d24949cccccceeeeeffffff'
+    Authorization: 'AWS4-HMAC-SHA256 Credential=ASIAVNINX4OVBSUDTHU4/20210729/us-west-2/healthlake/aws4_request, SignedHeaders=host;x-amz-date;x-amz-security-token, Signature=95ab2c54f7c7c851e3cc6c6e2406460caf3a2d24949cccccceeeeeffffff',
   };
 
   function mockedAws4Signer(options) {
@@ -39,7 +39,7 @@ describe('Client with request signer', function () {
   function mockAndCheckResponse(method) {
     const mock = nock(baseUrl)
       .matchHeader('accept', 'application/fhir+json');
-    let interceptor
+    let interceptor;
     if (method === 'GET') {
       interceptor = mock.get('/Patient/123');
     } else if (method === 'POST') {
